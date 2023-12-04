2f91207e4a1ecec6da6d26e6c1b0f6ab86aea8da
Christian Banzon <christian@digibuild.com>
2023-12-05 00:18:20 +0800

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import axios from "axios";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock Axios to prevent actual network requests
+jest.mock("axios");
+
+describe("App Component", () => {
+  // Mock the duties data
+  const mockDuties = [
+    { id: 1, name: "Duty 1" },
+    { id: 2, name: "Duty 2" },
+    // Add more duties as needed
+  ];
+
+  beforeEach(() => {
+    // Mock the Axios get function to resolve with the mockDuties
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
+      data: mockDuties,
+    });
+  });
+
+  test("renders duties and updates duty", async () => {
+    // Render the component
+    render(<App />);
+
+    // Ensure duties are rendered
+    await waitFor(() => {
+      mockDuties.forEach((duty) => {
+        expect(screen.getByText(duty.name)).toBeInTheDocument();
+      });
+    });
+
+    // Click on a duty to select it
+    fireEvent.click(screen.getByText("Duty 1"));
+
+    // Update the duty name
+    fireEvent.change(screen.getByLabelText("Duty Name:"), {
+      target: { value: "Updated Duty" },
+    });
+
+    // Submit the form to update the duty
+    fireEvent.click(screen.getByText("Update Duty"));
+
+    // Ensure the Axios PUT function was called with the correct parameters
+    expect(axios.put).toHaveBeenCalledWith("http://localhost:5000/api/data/1", {
+      name: "Updated Duty",
+    });
+
+    // Ensure the updated duties are rendered
+    await waitFor(() => {
+      expect(screen.getByText("Updated Duty")).toBeInTheDocument();
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import required modules
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+// Interface for Duty object
+interface Duty {
+  id: number;
+  name: string;
+}
+
+// React component
+const App: React.FC = () => {
+  // State for duties
+  const [duties, setDuties] = useState<Duty[]>([]);
+  // State to track the selected duty for editing
+  const [selectedDuty, setSelectedDuty] = useState<Duty | null>(null);
+  // State for form input
+  const [formData, setFormData] = useState({ name: "" });
+
+  // Effect to fetch duties from backend on component mount
+  useEffect(() => {
+    const fetchDuties = async () => {
+      try {
+        const response = await axios.get<Duty[]>(
+          "http://localhost:5000/api/data"
+        );
+        setDuties(response.data);
+      } catch (error) {
+        console.error("Error fetching duties", error);
+      }
+    };
+
+    fetchDuties();
+  }, []);
+
+  // Function to handle form input change
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Function to handle duty item click
+  const handleDutyClick = (selectedDuty: Duty) => {
+    setSelectedDuty(selectedDuty);
+    setFormData({ name: selectedDuty.name });
+  };
+
+  // Function to handle form submission for updating a duty
+  const handleUpdateDuty = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Form validation
+    if (!formData.name.trim()) {
+      alert("Duty name cannot be empty");
+      return;
+    }
+
+    try {
+      // Update the duty on the server
+      await axios.put(`http://localhost:5000/api/data/${selectedDuty?.id}`, {
+        name: formData.name,
+      });
+
+      // Refresh duties after updating
+      const response = await axios.get<Duty[]>(
+        "http://localhost:5000/api/data"
+      );
+      setDuties(response.data);
+
+      // Clear selected duty and form data
+      setSelectedDuty(null);
+      setFormData({ name: "" });
+    } catch (error) {
+      console.error("Error updating duty", error);
+    }
+  };
+
+  // Function to handle delete button click
+  const handleDeleteDuty = async () => {
+    try {
+      // Delete the duty on the server
+      await axios.delete(`http://localhost:5000/api/data/${selectedDuty?.id}`);
+
+      // Refresh duties after deleting
+      const response = await axios.get<Duty[]>(
+        "http://localhost:5000/api/data"
+      );
+      setDuties(response.data);
+
+      // Clear selected duty and form data
+      setSelectedDuty(null);
+      setFormData({ name: "" });
+    } catch (error) {
+      console.error("Error deleting duty", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Duties</h1>
+
+      {/* Render duties list */}
+      <ul>
+        {duties.map((duty) => (
+          <li key={duty.id} onClick={() => handleDutyClick(duty)}>
+            {duty.name}
+            <button onClick={handleDeleteDuty}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Form for updating duty */}
+      <form onSubmit={handleUpdateDuty}>
+        <label>
+          Duty Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Update Duty</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
